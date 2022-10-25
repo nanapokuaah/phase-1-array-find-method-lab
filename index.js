08ad2fae3229a5f1471a376e4dd252f3cd13bd89
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-10-25 19:11:04 -0400

    commit tree 8796f269b93f84544262eeb36fe66cba4992bbf0

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(array){
+    let record = array.find(element => element.result ==="W")
+        if (record){
+           return record.year
+        } else {
+            return undefined
+            
+        }    
+    }
+
